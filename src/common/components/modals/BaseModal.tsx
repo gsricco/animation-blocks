e93cec5a9233d9838c1c1bb43c98585570db7a94
sergio <gsricco@yandex.ru>
2024-02-19 14:54:32 +0300

    fix: fixed layout

--- src/common/components/modals/BaseModal.tsx
+++ src/common/components/modals/BaseModal.tsx
@@ -16,6 +16,7 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: '90%',
   height:'fit-content',
+  minHeight: '90%',
   bgcolor: `${baseTheme.colors.backgroundPage.dark}`,
   border: '2px solid #000',
   boxShadow: 24,
